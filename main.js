3d0ffaa6e0b3b12cd8dfddda434ea2e2cee67fe9
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2025-05-02 16:36:08 +0100

    Easter progress update
    -Added models from the labs
    -Created a logo
    -Created about page
    -Created final design for landing page

--- main.js
+++ main.js
@@ -1,39 +1,45 @@
 import * as THREE from "three";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.128/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.128/examples/jsm/controls/OrbitControls.js';
 
+// Initialisation
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
 const container = document.getElementById('threejs-container');
 const width = container.clientWidth;
 const height = container.clientHeight;
-const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-camera.position.z = 5;
-
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 const loader = new GLTFLoader();
-let bottleModel;
-loader.load("assets/bottle.glb", function (gltf) {
-    gltf.scene.scale.set(0.75, 0.75, 0.75);
-    const sections = {plastic: "Plastic", logo: "logo", cap: "Bottlecap"}
-    bottleModel = gltf.scene;
-    gltf.scene.traverse((child) => {
-        if (child.isMesh) {
-            console.log(child.name);
-            if (child.name === "Mesh") {
-                child.material = new THREE.MeshBasicMaterial({ color: "blue" });
-            }
-            if (child.name === "Mesh_2") {
-                child.material = new THREE.MeshBasicMaterial({ color: "blue" });
-            }
-        }
-    });
-    scene.add(gltf.scene);
-});
+const clock = new THREE.Clock();
+let activeModel;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
-document.getElementById('threejs-container').appendChild(renderer.domElement);
+function init() {
+    camera.position.z = 5;
+    scene.add(ambientLight);
+    scene.add(directionalLight);
+    renderer.setSize(width, height);
+    document.getElementById('threejs-container').appendChild(renderer.domElement);
+    console.log("init")
+}
+init();
+
+function loadModel(modelPath, scale) {
+    loader.load(modelPath, function (gltf) {
+        gltf.scene.scale.set(scale, scale, scale);
+        activeModel = gltf.scene;
+        const mixer = new THREE.AnimationMixer(activeModel);
+        gltf.scene.traverse((child) => {
+            if (child.isMesh) {
+                console.log(child.name);
+            }
+        });
+        scene.add(gltf.scene);
+    });
+}
+
+loadModel('assets/cup.glb', 0.75);
 
 window.addEventListener('resize', () => {
     const newWidth = container.clientWidth;
@@ -43,27 +49,18 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-
-const cube = new THREE.Mesh( geometry, material );
-cube.name = "Fred";
-
-scene.add( cube );
-cube.position.x = 2;
-
-const clock = new THREE.Clock();
-
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    if (bottleModel) {
+    if (activeModel) {
         const deltaTime = clock.getDelta();
-        bottleModel.rotation.x = Math.sin(clock.elapsedTime) * 0.5;
-        bottleModel.rotation.y += deltaTime * 0.5;
+        activeModel.rotation.x = Math.sin(clock.elapsedTime) * 0.5;
+        activeModel.rotation.y += deltaTime * 0.5;
     }
 	renderer.render( scene, camera );
 }
 
 renderer.setAnimationLoop( animate );
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(0, 0, 0);
+controls.update();
